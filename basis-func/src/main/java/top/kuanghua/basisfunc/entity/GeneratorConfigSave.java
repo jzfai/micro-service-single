8985485102dad9f5a755f1690135aa83d10548bc
kuanghua <869653722qq.com>
2022-10-20 11:51:27 +0800

    change some func config

--- basis-func/src/main/java/top/kuanghua/basisfunc/entity/GeneratorConfigSave.java
+++ basis-func/src/main/java/top/kuanghua/basisfunc/entity/GeneratorConfigSave.java
@@ -1,6 +1,9 @@
 package top.kuanghua.basisfunc.entity;
+
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -8,28 +11,34 @@ import lombok.Data;
 import java.io.Serializable;
 
 /**
-*  代码生成配置保存实体类
-*
-* @author 熊猫哥
-* @since 2022-07-20 10:10:28
-*/
+ * 代码生成配置保存实体类
+ *
+ * @author 熊猫哥
+ * @since 2022-07-20 10:10:28
+ */
 @Data
 @ApiModel("代码生成配置保存")
 @TableName(value = "t_config_save")
-public class GeneratorConfigSave extends Model< GeneratorConfigSave > {
+public class GeneratorConfigSave extends Model<GeneratorConfigSave> {
     @ApiModelProperty(value = "")
     private Integer id;
     @ApiModelProperty(value = "选中的字段配置")
     private String name;
     @ApiModelProperty(value = "生成的配置")
     private String generatorConfig;
-/**
-* 获取主键值
-*
-* @return 主键值
-*/
-@Override
-protected Serializable pkVal() {
-return this.id;
-}
+
+    @ApiModelProperty(value = "是否删除", hidden = true)
+    @TableLogic
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String deleted;
+
+    /**
+     * 获取主键值
+     *
+     * @return 主键值
+     */
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
 }
